8f65317810628b76a4bf9ddaa9829f5d82684d91
Robin Ting <civ2boss@gmail.com>
2024-08-04 22:20:07 -0400

    add oil.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,7 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  require('oil').setup(),
+  vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
+}
